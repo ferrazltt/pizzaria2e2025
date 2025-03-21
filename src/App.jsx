020bd1072dddf5eb498bfe68a94a86d585266969
ferrazltt <rm95227@estudante.fieb.edu.br>
2025-03-21 09:58:37 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ function App(){
 
   const [nome, setNome] = useState ('Ronaldo')
 
-  return (
-    <div>
-      <h3>Pizzaria 2E</h3>
-
-      <input
+ const Formulario = ()=>{
+ return(
+  <div>
+ <input
       className="nome" 
       onChange={(e)=>{setNome(e.target.value)}}
       type="text" />
@@ -19,7 +18,19 @@ function App(){
         >
           CLIQUE AQUI
       </button>
+  </div>
+ )
+}
+  return (
+    <div>
+      <h3>Pizzaria 2E</h3>
+
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
     </div>
+
   )
 }
 
